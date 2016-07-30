d06e8bb4d10103251b04c0731d9272141b9f6071
Pablo Munoz <pfariasmunoz@gmail.com>
2016-07-30 17:25:56 -0400

    add Bird class

--- core/src/com/pfariasmunoz/game/states/PlayState.java
+++ core/src/com/pfariasmunoz/game/states/PlayState.java
@@ -3,16 +3,17 @@ package com.pfariasmunoz.game.states;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.pfariasmunoz.game.FlappyDemo;
+import com.pfariasmunoz.game.sprites.Bird;
 
 /**
  * Created by Pablo Farias on 30-07-16.
  */
 public class PlayState extends State {
-    private Texture bird;
+    private Bird bird;
 
     public PlayState(GameStateManager gsm) {
         super(gsm);
-        bird = new Texture("bird.png");
+        bird = new Bird(50, 100);
         cam.setToOrtho(false, FlappyDemo.WIDTH / 2, FlappyDemo.HEIGHT / 2);
     }
 
@@ -23,16 +24,16 @@ public class PlayState extends State {
 
     @Override
     public void update(float dt) {
-
+        handleInput();
+        bird.update(dt);
     }
 
     @Override
     public void render(SpriteBatch sb) {
         sb.setProjectionMatrix(cam.combined);
         sb.begin();
-        sb.draw(bird, 50, 50);
+        sb.draw(bird.getTexture(), bird.getPosition().x, bird.getPosition().y);
         sb.end();
-
     }
 
     @Override
