147796b385c60dc226853eb4868f6ebf170a9005
Pablo Munoz <pfariasmunoz@gmail.com>
2016-07-31 00:38:04 -0400

    add sound and music

--- core/src/com/pfariasmunoz/game/FlappyDemo.java
+++ core/src/com/pfariasmunoz/game/FlappyDemo.java
@@ -2,8 +2,8 @@ package com.pfariasmunoz.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.pfariasmunoz.game.states.GameStateManager;
 import com.pfariasmunoz.game.states.MenuState;
@@ -16,10 +16,16 @@ public class FlappyDemo extends ApplicationAdapter {
 	private GameStateManager gsm;
 	private SpriteBatch batch;
 
+	private Music music;
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		gsm = new GameStateManager();
+        music = Gdx.audio.newMusic(Gdx.files.internal("music.mp3"));
+        music.setLooping(true);
+        music.setVolume(0.1f);
+        music.play();
         Gdx.gl.glClearColor(1, 0, 0, 1);
         gsm.push(new MenuState(gsm));
     }
@@ -31,8 +37,9 @@ public class FlappyDemo extends ApplicationAdapter {
         gsm.render(batch);
 	}
 	
-//	@Override
-//	public void dispose () {
-//		batch.dispose();
-//	}
+	@Override
+	public void dispose () {
+		super.dispose();
+        music.dispose();
+	}
 }
